b65668c30e8cf69bb0b8e57611cbf75254795a45
Kazbek <kaziksabanaev.2@gmail.com>
2026-08-07 17:20:52 +0300

    обновление получения заголовков страниц

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -8,14 +8,13 @@ import { MobileNav } from '@/shared/ui/NavBar/Mobile/';
 export const Layout = () => {
   const isMobile = useBreakpointValue({ base: true, md: false });
   const location = useLocation();
+  const title = Object.values(routes).find((r) => r.route === location.pathname)?.screenName ?? '';
   return (
-    <Flex flexDirection={isMobile ? 'column' : 'row'} position="relative" minH="100vh">
+    <Flex flexDirection={{ base: 'column', md: 'row' }} position="relative" minH="100vh">
       {isMobile ? <MobileNav /> : <DesktopNav />}
       {/* pb нужен потому что фиксированный элемент MobileNav перекрывает низ контента */}
-      <Box as="main" flex="1" bgColor="bg" p="5" pb={isMobile ? '64px' : undefined}>
-        <Heading fontSize={{ base: '18px', md: '22px' }}>
-          {location.pathname === '/' ? 'Главная' : routes[location.pathname.slice(1)].screenName}
-        </Heading>
+      <Box as="main" flex="1" bgColor="bg" p="5" pb={{ base: '64px' }}>
+        <Heading fontSize={{ base: '18px', md: '22px' }}>{title}</Heading>
         <Outlet />
       </Box>
     </Flex>
